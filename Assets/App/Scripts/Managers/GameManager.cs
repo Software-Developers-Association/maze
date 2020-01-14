2b3e50249b422e0ac246f74b57893561250bde49
Kevin Pacheco <k.pacheco@me.com>
2020-01-14 01:17:10 -0800

    Update: Added sample music, added timer.

--- Assets/App/Scripts/Managers/GameManager.cs
+++ Assets/App/Scripts/Managers/GameManager.cs
@@ -3,6 +3,7 @@
 public class GameManager : Singleton<GameManager> {
 	public static event System.Action<int> onScoreChanged = null;
 	private int score = 0;
+	private float seconds;
 
 	public static int Score {
 		get {
@@ -10,9 +11,16 @@ public class GameManager : Singleton<GameManager> {
 		}
 	}
 
+	public static float Seconds {
+		get {
+			return Instance.seconds;
+		}
+	}
+
 	public static void OnPickedUp(Pickup pickup) {
 		if(pickup is Pellet) {
 			Instance.score += 10;
+			Instance.seconds += 5.0F;
 
 			if(GameManager.onScoreChanged != null) {
 				GameManager.onScoreChanged(GameManager.Score);
@@ -27,6 +35,14 @@ public class GameManager : Singleton<GameManager> {
 	private MazeMaker maker = null;
 
 	private void Start() {
-		this.maker = Object.FindObjectOfType < MazeMaker>();
+		this.maker = Object.FindObjectOfType <MazeMaker>();
+
+		this.seconds = 30.0F;
+	}
+
+	private void Update() {
+		this.seconds -= Time.deltaTime;
+
+		if(this.seconds <= 0.0F) this.seconds = 0.0F;
 	}
 }
--- Assets/App/Scripts/UI/HUD.cs
+++ Assets/App/Scripts/UI/HUD.cs
@@ -2,8 +2,13 @@
 using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
+	static readonly string MinSec = "{0:00}:{1:00}";
+	static readonly string SecMil = "{0:00}:{1:000}";
+
 	[SerializeField]
 	private Text score;
+	[SerializeField]
+	private Text time;
 
 	private void Start() {
 		GameManager.onScoreChanged += GameManager_onScoreChanged;
@@ -11,5 +16,15 @@ public class HUD : MonoBehaviour {
 
 	private void GameManager_onScoreChanged(int score) {
 		this.score.text = score.ToString();
+	}
+
+	private void Update() {
+		var span = System.TimeSpan.FromSeconds(GameManager.Seconds);
+
+		if(span.Minutes > 0) {
+			this.time.text = string.Format(HUD.MinSec, span.Minutes, span.Seconds);
+		} else {
+			this.time.text = string.Format(HUD.MinSec, span.Minutes, span.Seconds);
+		}
 	}
 }
